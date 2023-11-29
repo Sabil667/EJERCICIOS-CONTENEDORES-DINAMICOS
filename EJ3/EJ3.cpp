74dc1c5b356d1bc7defea64d8bd2b57a7f188499
Sabil667 <msabi@myuax.com>
2023-11-29 19:39:21 +0100

    .

--- EJ3/EJ3.cpp
+++ EJ3/EJ3.cpp
@@ -7,7 +7,14 @@ using namespace std;
 
 int main(){
     vector<int> numeros= {44, 12, 67, 23, 87, 35, 54, 77, 92, 10};
-    cout<<
+    cout<<"Vector original: ";
+    for(const auto& num : numeros){
+        cout<<num<<" ";
+    }
+    cout<<endl;
+
+    sort(numeros.begin(), numeros.end());
+
 
 
 
