0392a6e45cd3f256aa8d7b7d0cc65699b8802b6c
Sabil667 <msabi@myuax.com>
2023-11-29 19:27:14 +0100

    .

--- EJ2/EJ2.cpp
+++ EJ2/EJ2.cpp
@@ -8,5 +8,7 @@ int main(){
     list<double> numeros = {1.1, 2.2, 3.3, 4.4, 5.5};
     stack<double, list<double>> numerosStack(numeros);
 
+    cout << "Lista original: ";
+    for(const auto&)
 
 }
