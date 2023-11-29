4af8567f75e3af3e2322048efdba31779bcbdac5
Sabil667 <msabi@myuax.com>
2023-11-29 19:07:06 +0100

    .

--- EJ1/EJ1.cpp
+++ EJ1/EJ1.cpp
@@ -6,6 +6,15 @@ using namespace std;
 int main(){
 vcetor<int> numeros;
 
+numeros.push_back(5);
+numeros.push_back(10);
+numeros.push_back(15);
+numeros.push_back(20);
+numeros.push_back(25);
+numeros.push_back(30);
+
+cout<<"Elementos del vector: "<<endl;
+
 
 
 
