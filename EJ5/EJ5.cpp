76a88367b070fe0fcc49fc43d99436b96a037e63
Sabil667 <msabi@myuax.com>
2023-11-29 20:24:01 +0100

    .

--- EJ5/EJ5.cpp
+++ EJ5/EJ5.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 int main(){
     vector<double> numeros = {1.0, 2.0, 3.0, 4.0, 5.0};
-    transform(numeros.begin(); numeros.end(), numeros.begin(), [](double num){return num*2;}));
+    transform(numeros.begin(), numeros.end(), numeros.begin(), [](double num){return num*2;});
 
     cout<< "Numeros originales: ";
     for(const auto& num : numeros){
